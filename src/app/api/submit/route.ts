0c5ffbc286bf8d86b7b0bc5af79484d600849575
zigga0398 <zack.flynn@mindspring.com>
2024-11-13 08:44:37 -0500

    tweaks and db pool

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -1,11 +1,24 @@
 // pages/api/submit.ts
+import pool from '../../db'
+
 export async function POST(req: Request) {
     const data = await req.json();
     console.log("body: " + JSON.stringify(data))
-    return new Response('', {
-        status: 200,
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
+    try {
+        await pool.query('INSERT INTO "submissions" (Data) VALUES ($1)', [data])
+        return new Response('', {
+            status: 200,
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+    } catch (e) {
+        console.error("Error: " + e)
+        return new Response('', {
+            status: 500,
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+    }
 }
--- src/app/components/rsvp-form.tsx
+++ src/app/components/rsvp-form.tsx
@@ -54,26 +54,26 @@ const RsvpForm: React.FC = () => {
                     <input type="text" name="name" id="full-name" placeholder="Full Name" value={nameValue} onChange={(e) => setNameValue(e.target.value)} required={true} className="p-2.5 w-full h-12 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     <h1 className="font-modernLoveCaps text-3xl">Will you be attending Zachary Flynn and Jamie Hunt&#39;s Wedding on October 25, 2025?</h1>
                     <label className="inline-flex items-center cursor-pointer">
-                        <input type="checkbox" name="rsvpStatus" id="attending" required={true} className="sr-only peer" value={rsvpStatus} onChange={(e) => setRsvpStatus(e.target.value)}/>
+                        <input type="checkbox" name="rsvpStatus" id="attending" required={true} className="sr-only peer" value={rsvpStatus.toString()} onChange={(e) => setRsvpStatus(e.target.value)}/>
                         <div
                             className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                        <span className="ms-3 text-sm text- font-modernLoveCaps text-gray-900 dark:text-gray-300">Yes/No</span>
+                        <span className="ms-3 text-sm font-modernLoveCaps text-gray-900 dark:text-gray-300">Yes/No</span>
                     </label>
                     <div className="flex flex-col justify-between">
                         <h1 className="font-modernLoveCaps text-3xl">Will you be bringing a plus one?</h1>
                         <label className="inline-flex items-center cursor-pointer">
-                            <input name="plusOne" type="checkbox" id="plusOne" className="sr-only peer" value={plusOneValue} onChange={(e) => setPlusOneValue(e.target.value)}/>
+                            <input name="plusOne" type="checkbox" id="plusOne" className="sr-only peer" value={plusOneValue.toString()} onChange={(e) => setPlusOneValue(String(e.target.value))}/>
                             <div
                                 className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                             <span
                                 className="ms-3 text-sm text- font-modernLoveCaps text-gray-900 dark:text-gray-300">Yes/No</span>
                         </label>
                     </div>
-                    <h1 className="font-modernLoveCaps text-3xl">Your Email</h1>
+                    <h1 className="font-modernLoveCaps text-3xl">Your Email&#9;<span className="text-sm">(Optional)</span></h1>
                     <input type="email" name="email" id="email" placeholder="your.email@domain.com" value={emailValue} onChange={(e) => setEmailValue(e.target.value)}
                            className="p-2.5 w-full h-12 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     <h1 className="font-modernLoveCaps text-3xl">Any additional comments or questions?</h1>
-                    <textarea name="message" id="message" rows={areaRows} value={messageValue} onChange={(e) => setMessageValue(e.target.value)}
+                    <textarea name="message" id="message" rows={areaRows} value={messageValue} onChange={(e) => setMessageValue(String(e.target.value))}
                               className="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                               placeholder="Write your thoughts here..."/>
                     <button type="submit"
